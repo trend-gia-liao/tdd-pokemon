68939d1d0a39c21078e953fcab4e41f5df6d7ba0
gia_liao <gia_liao@trendmicro.com>
2024-09-01 18:09:09 +0800

    add example

---
+++ trainer.go
@@ -0,0 +1,176 @@
+package tdd_pokemon
+
+import "fmt"
+
+type Trainer struct {
+	storage Storage
+}
+
+type Storage struct {
+	pokemon []Pokemon
+	sizeMax int
+}
+
+func (s *Storage) add(pokemon Pokemon) {
+	s.pokemon = append(s.pokemon, pokemon)
+}
+
+func (s *Storage) getLeftSpace() int {
+	return s.sizeMax - len(s.pokemon)
+}
+
+func (s *Storage) CountPVP(no int) int {
+	count := 0
+	for _, pokemon := range s.pokemon {
+		if pokemon.no == no && pokemon.iv.isPVP() {
+			count++
+		}
+	}
+	return count
+}
+
+func (s *Storage) CountBetter(pokemon Pokemon) int {
+	count := 0
+	for _, p := range s.pokemon {
+		if p.no == pokemon.no && p.iv.Sum() > pokemon.iv.Sum() {
+			count++
+		}
+	}
+	return count
+}
+
+func (s *Storage) CountShinyBetter(pokemon Pokemon) int {
+	count := 0
+	for _, p := range s.pokemon {
+		if p.no == pokemon.no && p.isShiny() && p.iv.Sum() > pokemon.iv.Sum() {
+			count++
+		}
+	}
+	return count
+}
+
+type Pokemon struct {
+	no    int
+	iv    IV
+	shiny bool
+	event bool
+}
+
+var legendaryNo = []int{144, 145, 146, 151}
+
+func (p *Pokemon) isLegendary() bool {
+	for _, no := range legendaryNo {
+		if p.no == no {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Pokemon) isShiny() bool {
+	return p.shiny
+}
+
+func (p *Pokemon) isEvent() bool {
+	return p.event
+}
+
+type IV struct {
+	Attack  int
+	Defense int
+	HP      int
+}
+
+func (iv *IV) Sum() int {
+	return iv.Attack + iv.Defense + iv.HP
+}
+
+func (iv *IV) isThreeStar() bool {
+	return iv.Sum() >= 37
+}
+
+func (iv *IV) isExcellent() bool {
+	return iv.Sum() >= 42
+}
+
+func (iv *IV) isPVP() bool {
+	return iv.Attack < iv.Sum()/3
+}
+
+func NewTrainer() *Trainer {
+	return &Trainer{storage: Storage{sizeMax: 1000}}
+}
+
+func (t *Trainer) Play() error {
+	if t.storage.getLeftSpace() <= 0 {
+		return fmt.Errorf("your storage is full. Transfer pokemon or upgrade storage")
+	}
+
+	pokemon, index := t.Gotcha()
+	if t.QuickFilter(pokemon) == false {
+		_ = t.Transfer(index)
+	}
+
+	return nil
+}
+
+func (t *Trainer) Manage() {
+	for i := len(t.storage.pokemon) - 1; i >= 0; i-- {
+		pokemon := t.storage.pokemon[i]
+		if t.PreciseFilter(pokemon) == false {
+			_ = t.Transfer(i)
+		}
+	}
+}
+
+func (t *Trainer) QuickFilter(pokemon Pokemon) bool {
+	if pokemon.iv.isThreeStar() {
+		return true
+	}
+	if pokemon.iv.isPVP() {
+		return true
+	}
+	if pokemon.isLegendary() {
+		return true
+	}
+	if pokemon.isShiny() {
+		return true
+	}
+	if pokemon.isEvent() {
+		return true
+	}
+	return false
+}
+
+func (t *Trainer) PreciseFilter(pokemon Pokemon) bool {
+	if pokemon.iv.isExcellent() {
+		return true
+	}
+	if pokemon.iv.isPVP() && t.storage.CountPVP(pokemon.no) <= 1 {
+		return true
+	}
+	if pokemon.isLegendary() && t.storage.CountBetter(pokemon) <= 2 {
+		return true
+	}
+	if pokemon.isShiny() && t.storage.CountShinyBetter(pokemon) <= 0 {
+		return true
+	}
+	if pokemon.isShiny() && pokemon.isLegendary() && t.storage.CountShinyBetter(pokemon) <= 2 {
+		return true
+	}
+	return false
+}
+
+func (t *Trainer) Gotcha() (Pokemon, int) {
+	pokemon := Pokemon{}
+	t.storage.add(pokemon)
+	return pokemon, len(t.storage.pokemon) - 1
+}
+
+func (t *Trainer) Transfer(index int) error {
+	if index < 0 || index >= len(t.storage.pokemon) {
+		return fmt.Errorf("index out of range")
+	}
+	t.storage.pokemon = append(t.storage.pokemon[:index], t.storage.pokemon[index+1:]...)
+	return nil
+}
---
+++ trainer_test.go
@@ -0,0 +1,181 @@
+package tdd_pokemon
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type TrainerTestSuite struct {
+	suite.Suite
+	Trainer
+}
+
+func (suite *TrainerTestSuite) SetupTest() {
+	suite.Trainer = *NewTrainer()
+}
+
+func (suite *TrainerTestSuite) TestPlayWithFullStorage() {
+	suite.Trainer.storage.pokemon = make([]Pokemon, 1000)
+	err := suite.Trainer.Play()
+	suite.Error(err)
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterIVTrue() {
+	pokemon := Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 13}}
+	suite.True(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterIVFalse() {
+	pokemon := Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 12}}
+	suite.False(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterPVPTrue() {
+	pokemon := Pokemon{iv: IV{Attack: 6, Defense: 12, HP: 12}}
+	suite.True(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterPVPFalse() {
+	pokemon := Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 6}}
+	suite.False(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterLegendaryTrue() {
+	pokemon := Pokemon{no: 144}
+	suite.True(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterLegendaryFalse() {
+	pokemon := Pokemon{no: 1}
+	suite.False(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterShinyTrue() {
+	pokemon := Pokemon{no: 25, shiny: true}
+	suite.True(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterShinyFalse() {
+	pokemon := Pokemon{no: 25, shiny: false}
+	suite.False(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterEventTrue() {
+	pokemon := Pokemon{no: 25, event: true}
+	suite.True(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestQuickFilterEventFalse() {
+	pokemon := Pokemon{no: 25, event: false}
+	suite.False(suite.Trainer.QuickFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterIVTrue() {
+	pokemon := Pokemon{iv: IV{Attack: 14, Defense: 14, HP: 14}}
+	suite.True(suite.Trainer.PreciseFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterIVFalse() {
+	pokemon := Pokemon{iv: IV{Attack: 13, Defense: 13, HP: 13}}
+	suite.False(suite.Trainer.PreciseFilter(pokemon))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterPVPTrue() {
+	suite.Trainer.storage.add(Pokemon{no: 1, iv: IV{Attack: 3, Defense: 14, HP: 14}})
+	suite.Trainer.storage.add(Pokemon{no: 979, iv: IV{Attack: 2, Defense: 15, HP: 15}})
+	suite.True(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterPVPFalse() {
+	suite.Trainer.storage.add(Pokemon{no: 979, iv: IV{Attack: 3, Defense: 14, HP: 14}})
+	suite.Trainer.storage.add(Pokemon{no: 979, iv: IV{Attack: 2, Defense: 15, HP: 15}})
+	suite.False(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterLegendaryTrue() {
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 14, Defense: 14, HP: 14}})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 15, Defense: 15, HP: 15}})
+	suite.True(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterLegendaryFalse() {
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 12, Defense: 12, HP: 12}})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 14, Defense: 14, HP: 14}})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 15, Defense: 15, HP: 15}})
+	suite.False(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterShinyTrue() {
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 11, Defense: 11, HP: 11}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 12, Defense: 12, HP: 12}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	suite.True(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[1]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterShinyFalse() {
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 11, Defense: 11, HP: 11}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 12, Defense: 12, HP: 12}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 25, iv: IV{Attack: 13, Defense: 13, HP: 13}, shiny: true})
+	suite.False(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterShinyLegendaryTrue() {
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 11, Defense: 10, HP: 10}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 11, Defense: 11, HP: 11}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 12, Defense: 12, HP: 12}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	suite.True(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestPreciseFilterShinyLegendaryFalse() {
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 10, Defense: 10, HP: 10}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 11, Defense: 11, HP: 11}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 12, Defense: 12, HP: 12}, shiny: true})
+	suite.Trainer.storage.add(Pokemon{no: 144, iv: IV{Attack: 13, Defense: 13, HP: 13}, shiny: true})
+	suite.False(suite.Trainer.PreciseFilter(suite.Trainer.storage.pokemon[0]))
+}
+
+func (suite *TrainerTestSuite) TestManage() {
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 12}})
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 14, Defense: 14, HP: 14}})
+	suite.Trainer.Manage()
+	suite.Equal(1, len(suite.Trainer.storage.pokemon))
+	suite.Equal(Pokemon{iv: IV{Attack: 14, Defense: 14, HP: 14}}, suite.Trainer.storage.pokemon[0])
+}
+
+func (suite *TrainerTestSuite) TestGotcha() {
+	p, i := suite.Trainer.Gotcha()
+	suite.IsType(Pokemon{}, p)
+	suite.Equal(1, len(suite.Trainer.storage.pokemon))
+	suite.Equal(0, i)
+}
+
+func (suite *TrainerTestSuite) TestTransferSuccess() {
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 12}})
+	err := suite.Trainer.Transfer(0)
+	suite.NoError(err)
+	suite.Equal(0, len(suite.Trainer.storage.pokemon))
+}
+
+func (suite *TrainerTestSuite) TestTransferTwiceSuccess() {
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 12}})
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 14, Defense: 14, HP: 14}})
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 13, Defense: 13, HP: 13}})
+	_ = suite.Trainer.Transfer(2)
+	_ = suite.Trainer.Transfer(0)
+	suite.Equal(1, len(suite.Trainer.storage.pokemon))
+	suite.Equal(Pokemon{iv: IV{Attack: 14, Defense: 14, HP: 14}}, suite.Trainer.storage.pokemon[0])
+}
+
+func (suite *TrainerTestSuite) TestTransferFail() {
+	suite.Trainer.storage.add(Pokemon{iv: IV{Attack: 12, Defense: 12, HP: 12}})
+	err := suite.Trainer.Transfer(1)
+	suite.Error(err)
+}
+
+func TestTrainerTestSuite(t *testing.T) {
+	suite.Run(t, new(TrainerTestSuite))
+}
